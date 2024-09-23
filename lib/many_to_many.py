797d5118043ffcedaa53f8b9cd32d5bb0b280b7b
Kush <kuriaisac@gmail.com>
2024-09-23 15:04:40 +0300

    updates changes

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,37 @@
 class Author:
-    pass
+    
+    def __init__(self, name):
+        if isinstance(name, str):
+            self.name = name
+        self.contract = []
+
+    def contracts(self):
+        return [contracts for contracts in Contract.all]
 
 
 class Book:
-    pass
+   
+    def __init__(self, title):
+        if isinstance(title, str):
+            self.title = title
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+            if not isinstance(author, Author):
+                raise TypeError('author is not an instance of Author')
+            if not isinstance(book, Book):
+                raise TypeError('book is not an instance of Book')
+            if not isinstance(date, str):
+                raise TypeError('date is should be a string')
+            if not isinstance(royalties, int):
+                raise TypeError('royalties should be a number')
+            
+            self.author = author
+            self.book = book
+            self.date = date
+            self.royalties = royalties
+            Contract.all.append(self)
+
+            
